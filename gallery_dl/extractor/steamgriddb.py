6bcf331c3ca19f7e2b018ba8a7abb64e3145c85f
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 15:37:47 +0000

    waleko__gemini-pro-1.5_8afe__id_114

--- gallery_dl/extractor/steamgriddb.py
+++ gallery_dl/extractor/steamgriddb.py
@@ -9,33 +9,35 @@
 from .common import Extractor, Message
 from .. import text, exception
 
-
 BASE_PATTERN = r"(?:https?://)?(?:www\.)?steamgriddb\.com"
 LANGUAGE_CODES = (
-    "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az",
-    "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs", "ca", "ce",
-    "ch", "co", "cr", "cs", "cu", "cv", "cy", "da", "de", "dv", "dz", "ee",
-    "el", "en", "eo", "es", "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr",
-    "fy", "ga", "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr",
-    "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik", "io", "is",
-    "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj", "kk", "kl", "km", "kn",
-    "ko", "kr", "ks", "ku", "kv", "kw", "ky", "la", "lb", "lg", "li", "ln",
-    "lo", "lt", "lu", "lv", "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms",
-    "mt", "my", "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv",
-    "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps", "pt", "qu",
-    "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd", "se", "sg", "si", "sk",
-    "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw", "ta",
-    "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw",
-    "ty", "ug", "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi",
+    "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av",
+    "ay", "az", "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo",
+    "br", "bs", "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv",
+    "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo", "es",
+    "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga",
+    "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr",
+    "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik",
+    "io", "is", "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj",
+    "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw",
+    "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv",
+    "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my",
+    "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv",
+    "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps",
+    "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd",
+    "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr",
+    "ss", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti",
+    "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug",
+    "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi",
     "yo", "za", "zh", "zu",
 )
 FILE_EXT_TO_MIME = {
-    "png": "image/png",
+    "png":  "image/png",
     "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
+    "jpg":  "image/jpeg",
     "webp": "image/webp",
-    "ico": "image/vnd.microsoft.icon",
-    "all": "all",
+    "ico":  "image/vnd.microsoft.icon",
+    "all":  "all",
 }
 
 
